0fc1c1d974bf86bed56def697ddebf0c2c912e10
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2019-01-02 01:51:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    
-    Time.now.strftime("%H").to_i >= 12 ? resp.write "Good Afternoon!" : resp.write "Good Morning!"
+    current_time = Time.now
+    current_time.hour >= 12 ? resp.write "Good Afternoon!" : resp.write "Good Morning!"
     resp.finish
   end
 
